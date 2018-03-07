26ecda65a968801d7727f2c576526bb5ff40e1ee
amitnagesh <amitnagesh@gmail.com>
2018-03-06 23:34:23 -0800

    Add files via upload

--- test1.py
+++ test1.py
@@ -4,7 +4,16 @@ u = urlopen('http://jsonplaceholder.typicode.com/albums/')
 resp = json.loads(u.read().decode('utf-8'))
 
 from pprint import pprint
+pprint ("---------")
 pprint (resp)
+pprint ("---------")
 
 for i in resp:
-    print i["userId"]
+    if "adipisci laborum fuga laboriosam" in i["title"]:
+        print i["id"]
+    if "adipisci laborum fuga laboriosam" not in i["title"]:
+        continue
+pprint ("---------")        
+
+        
+        
