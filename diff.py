a08d56766589ed2ffd62b53a8704238aee7ad58f
amitnagesh <amitnagesh@gmail.com>
2020-04-05 09:41:21 -0700

    Added extra prints for separate

--- diff.py
+++ diff.py
@@ -12,6 +12,9 @@ def readFileToList(file):
     file_content_list = [line.replace(' ', '') for line in lines] # Replace white space from each line
     return file_content_list
 
+def common(lst1, lst2): 
+    return list(set(lst1) & set(lst2))
+
 file_name_1 = sys.argv[1]
 file_name_2 = sys.argv[2]
 master_list = readFileToList(file_name_1)
@@ -23,5 +26,13 @@ if not diff_list:
 else:
     print (file_name_1 + " and " + file_name_2 + " contents are NOT same\n")
 
-print (diff_list)   #print diff as list 
-# print (listToDict(diff_list)) #print diff as dict
+print ("element not present on both the files")
+print (diff_list)   #print diff as list
+print ("\n")
+
+print ("element presents only on " + file_name_1 +"\n")
+print (common(diff_list, master_list))
+print ("\n")
+
+print ("element presents only on " + file_name_2 +"\n")
+print (common(diff_list, slave_list))
