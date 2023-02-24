a3be6d186bdca093e18c7e3269edcbe92650435c
amitnagesh <amitnagesh@gmail.com>
2023-02-24 09:45:21 -0800

    Adding filter in loop

--- diffUtility.py
+++ diffUtility.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+#import re
 
 # below script will extract keywords (line) from file1 
 # search in file2
@@ -15,6 +16,13 @@ result = open('result.txt', 'w')
 # iterate over file1
 with open('file1.txt') as file1:
     for line in file1:
+        
+        #if re.search('Merge', line): # with re import
+        #    continue 
+        
+        #if 'Merge' in line:     # with if condition
+        #    continue
+
         words = line.split()
         commitId = "".join(words[0:1]) #extract commitId
         key = " ".join(words[1:]) #remove commitId from line
